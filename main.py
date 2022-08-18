0015ddbd22aad21b4a34dd7398a8632b21eba168
Edward Arias <79659253+jaysongraphics@users.noreply.github.com>
2022-08-18 01:02:59 +0000

    clean spaces

--- main.py
+++ main.py
@@ -5,13 +5,9 @@ random_number = random.randint(1, 20)
 
 random_float = random.random()
 
-
 random_love_score = random.randint(1, 20)
 
-
-
 print(f"Your love score is {random_number}")
-
 print(random_number)
 print(random_float)
 print(pi.pi_number)
