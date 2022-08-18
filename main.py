d7ee6f16bb84b26ea0a24ddb34f01020847fbccd
Edward Arias <79659253+jaysongraphics@users.noreply.github.com>
2022-08-18 00:59:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import random 
+
+random_number = random.randint(1, 20)
+
+random_float = random.random()
+
+
+random_love_score = random.randint(1, 20)
+
+
+
+print(f"Your love score is {random_number}")
+
+print(random_number)
+print(random_float)
